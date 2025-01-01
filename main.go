4a83cf4922a6995b18c28384597d98027c887c72
Marco Antonio - markitos DevSecOps Culture <markitos.es.info@gmail.com>
2025-01-01 11:46:49 +0100

    - por el momento el compose se queda con postgres solo

--- main.go
+++ main.go
@@ -23,10 +23,10 @@ func main() {
 		log.Fatal(err)
 	}
 
+	server := loadServer(config, repository)
 	log.Println("['.']:>--- </starting markitos-golang-service-boilerplate>")
 	log.Println("['.']:>--------------------------------------------")
 	log.Println("['.']:>")
-	server := loadServer(config, repository)
 	err = server.Run()
 	if err != nil {
 		log.Fatal("unable to start server: ", err)
@@ -34,17 +34,13 @@ func main() {
 }
 
 func loadServer(config configuration.MarkitosGolangServiceBoilerplateConfig, repository *database.BoilerPostgresRepository) *api.Server {
-	log.Println("['.']:>----- <server.api>")
 	gin.SetMode(gin.ReleaseMode)
 	server := api.NewServer(config.AppAddress, repository)
 	log.Println("['.']:>------- New server created")
-	log.Println("['.']:>----- </server.api>")
 	return server
 }
 
 func loadDatabase(config configuration.MarkitosGolangServiceBoilerplateConfig) (*database.BoilerPostgresRepository, error) {
-	log.Println("['.']:>")
-	log.Println("['.']:>----- <database>")
 	db, err := gorm.Open(postgres.Open(config.DsnDatabase), &gorm.Config{})
 	if err != nil {
 		log.Fatal("['.']:> error unable to connect to database:", err)
@@ -55,23 +51,17 @@ func loadDatabase(config configuration.MarkitosGolangServiceBoilerplateConfig) (
 	}
 	repository := database.NewBoilerPostgresRepository(db)
 	log.Println("['.']:>------- Connected to database - migrations")
-	log.Println("['.']:>----- </database>")
-	log.Println("['.']:>")
 
 	return repository, nil
 }
 
 func loadConfiguration() configuration.MarkitosGolangServiceBoilerplateConfig {
-	log.Println("['.']:>")
-	log.Println("['.']:>----- <configuration>")
 	config, err := configuration.LoadConfiguration(".")
 	if err != nil {
 		log.Fatal("['.']:>------- unable to load configuration: ", err)
 	}
 	log.Println("['.']:>------- all values ready to use :)")
 	log.Println("['.']:>------- serverAddress: ", config.AppAddress)
-	log.Println("['.']:>----- </configuration>")
-	log.Println("['.']:>")
 
 	return config
 }
